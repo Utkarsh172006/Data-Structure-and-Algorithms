d798c5f1b102123b0b70f63392cbd101802a8f1d
Utkarsh Kumar Srivastava <128831824+Utkarsh172006@users.noreply.github.com>
2026-08-11 17:00:08 +0530

    Time: 2 ms (18.6%), Space: 43.7 MB (10.48%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -2,6 +2,8 @@ class Solution {
     public void sortColors(int[] nums) {
         int low = 0, mid = 0, high = nums.length - 1;
 
+        // Used Dutch National Flag Algorithm (or) DNF Algorithm
+
         while(mid <= high){
             if(nums[mid] == 0){
                 int temp = nums[mid];
